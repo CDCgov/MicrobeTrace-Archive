CDCgov/MicrobeTrace-Archive

commit 21cade6a6224e5390ec4bb435f0e91036c6febc6

Set Background Thread to Kill all Viz Windows on New Session

src/background.js
@@ -134,6 +134,12 @@ ipcMain.on('launch-view', (event, view) => {
   }));
 });
 
-ipcMain.on('reset', reset);
+ipcMain.on('reset', () => {
+  BrowserWindow
+    .getAllWindows()
+    .filter(w => w.id != mainWindow.id)
+    .forEach(w => w.close());
+  reset();
+});
 
 app.on('window-all-closed', app.quit);

commit 0263b4aa2043d3892859ec807293ba815d8c25ab

Fixed background thread error
Corrects error introduced in 21cade6a6224e5390ec4bb435f0e91036c6febc6

src/background.js
@@ -4,7 +4,7 @@ import jetpack from 'fs-jetpack';
 import { app, BrowserWindow, ipcMain } from 'electron';
 import createWindow from './helpers/window';
 
-var data;
+var data, mainWindow;
 
 function reset(){
   data = {
@@ -22,7 +22,7 @@ const manifest = jetpack.cwd(app.getAppPath()).read('package.json', 'json');
 ipcMain.on('log', (event, msg) => console.log(msg));
 
 app.on('ready', () => {
-  const mainWindow = createWindow('main', {
+  mainWindow = createWindow('main', {
     width: 1024,
     height: 768,
     show: true