6f1597caba4901057912f639ffd4980e4ef04a6f
Tony Boyles <Anthony@Boyles.cc>
2018-02-15 14:58:54 -0500

    Moved Manifest into session

--- src/background.js
+++ src/background.js
@@ -5,6 +5,8 @@ import { app, BrowserWindow, ipcMain } from 'electron';
 import createWindow from './helpers/window';
 import _ from 'lodash';
 
+const manifest = jetpack.cwd(app.getAppPath()).read('package.json', 'json');
+
 function dataSkeleton(){
   return {
     files: [],
@@ -26,15 +28,14 @@ function dataSkeleton(){
       linkcolors: ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"],
       nodecolors: ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"]
     },
-    messages: []
+    messages: [],
+    manifest: manifest
   };
 };
 
 var session = dataSkeleton();
 
 var mainWindow, parserWindow, components = {};
-
-const manifest = jetpack.cwd(app.getAppPath()).read('package.json', 'json');
 
 ipcMain.on('log', (event, msg) => console.log(msg));
 
